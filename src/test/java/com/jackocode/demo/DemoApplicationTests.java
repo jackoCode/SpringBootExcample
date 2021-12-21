32d7e659e9cf054b144c3753d8ef34c93c2c73e4
Sascha Schwiewagner <saschaschwiewagner@MacBook-Pro-von-Sascha.fritz.box>
2021-12-21 17:46:14 +0100

    Initial commit

---
+++ src/test/java/com/jackocode/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackocode.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
